b9a8609b2ad94ce0c6a679ddc3088d0bc5b59e59
Jason Harris <jiharris90@gmail.com>
2016-09-05 00:15:01 +0100

    Updating Main to output result of shopping cart

--- src/main/scala/com/github/harl0/shoppingcart/Main.scala
+++ src/main/scala/com/github/harl0/shoppingcart/Main.scala
@@ -3,8 +3,9 @@ package com.github.harl0.shoppingcart
 /**
   * Created by Jason on 03/09/2016.
   */
-object HelloWorld {
-  def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+object Main {
+  def main(args: Array[String]) {
+    println("[ " + args.mkString(", ") + " ] => £" + ShoppingCart.checkout(args))
+//    println("Hello, world!")
   }
 }
